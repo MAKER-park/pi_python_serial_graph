07f2c64518efb77f3d17702a5af745f133447bd1
park jae wan <hanulcom7@naver.com>
2020-01-10 07:16:19 +0900

    peer to peer rf onbyte send
    1:1 com

--- arduino/keywish-nano-plus-master/RF-Nano/RF-NANO Demo Program/Peer-to-peer Communication/OneByteTest/Receive/Receive.ino
+++ arduino/keywish-nano-plus-master/RF-Nano/RF-NANO Demo Program/Peer-to-peer Communication/OneByteTest/Receive/Receive.ino
@@ -15,14 +15,14 @@ void setup()
   Mirf.payload = sizeof(value);
   Mirf.channel = 90;             //Set the used channel
   Mirf.config();
-  Serial.println("Listening...");  //Start listening to received data
+  //Serial.println("Listening...");  //Start listening to received data
 }
 
 void loop()
 {
   if (Mirf.dataReady()) { //When the program is received, the received data is output from the serial port
     Mirf.getData(&value);
-    Serial.print("Got MotorDrive data: ");
+    //Serial.print("Got MotorDrive data: ");
     Serial.println(value);
   }
 }
