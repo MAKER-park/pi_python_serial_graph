36daf503c8ce8e3d30ec5461e5a29d2b18614fc5
pjw <pjw970219@gmail.com>
2020-02-05 08:14:50 +0900

    1on1
    나중에 서버 단에서 멀티 클라이언트 내용 받는 걸로 수정할것

---
+++ arduino_sensor/SENSOR_WIFI/client_test_wifi/client_test_wifi.ino
@@ -0,0 +1,91 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp8266-client-server-wi-fi/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti WiFiMulti;
+
+const char* ssid = "ESP8266-Access-Point";
+const char* password = "123456789";
+
+//Your IP address or domain name with URL path
+const char* serverNameTemp = "http://192.168.4.1/temperature";
+
+
+#include <Wire.h>
+
+String temperature;
+
+
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  
+ 
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP(ssid, password);
+  while((WiFiMulti.run() == WL_CONNECTED)) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("Connected to WiFi");
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+}
+
+void loop() {
+
+     // Check WiFi connection status
+    if ((WiFiMulti.run() == WL_CONNECTED)) {
+      temperature = httpGETRequest(serverNameTemp);
+      Serial.println("Temperature: " + temperature );
+
+      
+      // save the last HTTP GET Request
+    }
+    else {
+      Serial.println("WiFi Disconnected");
+    }
+}
+
+String httpGETRequest(const char* serverName) {
+  WiFiClient client;
+  HTTPClient http;
+    
+  // Your IP address with path or Domain name with URL path 
+  http.begin(client, serverName);
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.GET();
+  
+  String payload = "--"; 
+  digitalWrite(LED_BUILTIN, LOW);  
+  
+  if (httpResponseCode>0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    payload = http.getString();
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else {
+    Serial.print("Error code: ");
+    Serial.println(httpResponseCode);
+  }
+  // Free resources
+  http.end();
+
+  return payload;
+}
---
+++ arduino_sensor/SENSOR_WIFI/server_test_wifi/server_test_wifi.ino
@@ -0,0 +1,58 @@
+/*
+  Rui Santos
+  Complete project details at https://RandomNerdTutorials.com/esp8266-nodemcu-client-server-wi-fi/
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files.
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+*/
+
+// Import required libraries
+#include <ESP8266WiFi.h>
+#include "ESPAsyncWebServer.h"
+
+#include <Wire.h>
+
+
+// Set your access point network credentials
+const char* ssid = "ESP8266-Access-Point";
+const char* password = "123456789";
+
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+String readTemp() {
+  String abc = String(random(300));
+  return abc;
+  //return String(1.8 * bme.readTemperature() + 32);
+}
+
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  Serial.println();
+  
+  // Setting the ESP as an access point
+  Serial.print("Setting AP (Access Point)…");
+  // Remove the password parameter, if you want the AP (Access Point) to be open
+  WiFi.softAP(ssid, password);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(IP);
+
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/plain", readTemp().c_str());
+  });
+  
+  // Start server
+  server.begin();
+}
+ 
+void loop(){
+  
+}
